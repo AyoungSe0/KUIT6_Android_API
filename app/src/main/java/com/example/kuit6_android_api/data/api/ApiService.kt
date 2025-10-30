ec88cfe260d24c1a00024db3dee3006bd6663895
AyoungSe0 <ayoungseo1005@gmail.com>
2025-10-30 21:41:46 +0900

    5주차 미션 완료

---
+++ app/src/main/java/com/example/kuit6_android_api/data/api/ApiService.kt
@@ -0,0 +1,42 @@
+package com.example.kuit6_android_api.data.api
+
+
+import com.example.kuit6_android_api.data.model.request.PostCreateRequest
+import com.example.kuit6_android_api.data.model.response.BaseResponse
+import com.example.kuit6_android_api.data.model.response.PostResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface ApiService {
+    // http://3.34.136.227:8080/api/posts
+    @GET(value = "/api/posts")
+    suspend fun getPosts(): BaseResponse<List<PostResponse>> // suspend fun: 일시 중단 될 수도 있는 함수
+
+    // http://3.34.136.227:8080/api/posts
+    @POST(value = "/api/posts")
+    suspend fun createPost(
+        @Query(value = "author") author: String = "anonymous",
+        @Body request: PostCreateRequest
+    ): BaseResponse<PostResponse>
+
+    @GET(value = "/api/posts/{id}")
+    suspend fun getDetail(
+        @Path("id") id: Long
+    ): BaseResponse<PostResponse>
+
+    @PUT(value = "/api/posts/{id}")
+    suspend fun updatePost(
+        @Path(value = "id") id: Long,
+        @Body request: PostCreateRequest
+    ): BaseResponse<PostResponse>
+
+    @DELETE(value = "/api/posts/{id}")
+    suspend fun deletePost(
+        @Path("id") id: Long
+    ): BaseResponse<Unit> // 서버가 data 없으면 Unit/Any? 로 받기
+}
--- app/src/main/java/com/example/kuit6_android_api/ui/post/screen/PostCreateScreen.kt
+++ app/src/main/java/com/example/kuit6_android_api/ui/post/screen/PostCreateScreen.kt
@@ -187,8 +187,8 @@ fun PostCreateScreen(
 
             Button(
                 onClick = {
-                    val finalAuthor = author.ifBlank { "anonymous" }
-                    viewModel.createPost(finalAuthor, title, content, null) {
+                    val finalAuthor = author//.ifBlank { "anonymous" }
+                    viewModel.createPost(finalAuthor, title, content, null/*viewModel.uploadedImage*/) {
                         onPostCreated()
                     }
                 },
